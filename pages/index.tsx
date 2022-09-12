745964ff15e6a69af673eb82057e9975cfe47f77
LucasDarela <lucasdarela@.com>
2022-09-12 15:00:24 -0300

    testee

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,12 @@
 import Head from "next/head";
 import Landing from "../components/Landing";
-// import Diferentials from "../components/Diferentials";
 import Chopes from "../components/Chopes";
-// import Featured from '../components/Featured'
-
-
-// interface Props {
-//   categories: Category[];
-//   products: Product[];
-// }
 
 const Home = ({ }) => {
   return (
     <div className="">
       <Head>
-        <title>Darela Chopp</title>
+        <title>Darela Chopp Express</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Landing />
